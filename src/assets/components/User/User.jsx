b1ce417e9aa4d170b25083e6883cc6ba386d3445
Imamasharmeen <imamasharmeen@gmail.com>
2024-10-29 13:46:35 +0200

    Fetch user data using Loader Property

---
+++ src/assets/components/User/User.jsx
@@ -0,0 +1,10 @@
+
+
+export default function User({user}) {
+    const{name, email, address} =user
+  return (
+    <div>
+      <h2>{name}</h2>
+    </div>
+  )
+}
--- src/assets/components/Users.jsx
+++ src/assets/components/Users.jsx
@@ -1,9 +1,17 @@
-
+import { useLoaderData } from "react-router-dom"
+import User from "./User/User"
 
 export default function Users() {
+    const users = useLoaderData()
+    console.log(users)
   return (
     <div>
-      <h2>Happy Users</h2>
+      <h2>Happy Users: {users.length}</h2>
+      <div>
+        {
+            users.map(user=><User key={user.id} user={user}></User>)
+        }
+      </div>
     </div>
   )
 }
